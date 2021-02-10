a84391cc032a51eda9066fe8c54dbb88bf8d917a
Julius Kiano <jgkiano@gmail.com>
2021-02-10 23:40:36 +0300

    feature, starting SmsRetriever and getting app signatures

--- android/src/main/java/com/reactnativeandroidsmsverificationapi/AndroidSmsVerificationApiModule.java
+++ android/src/main/java/com/reactnativeandroidsmsverificationapi/AndroidSmsVerificationApiModule.java
@@ -11,9 +11,18 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.android.gms.auth.api.credentials.Credential;
 import com.google.android.gms.auth.api.credentials.Credentials;
 import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
 
 public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule {
 
@@ -30,9 +39,11 @@ public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule 
 
   private int phoneNumberRequestCode = 420;
   private Promise promise;
+  private static ReactApplicationContext reactContext;
 
   AndroidSmsVerificationApiModule(ReactApplicationContext context) {
     super(context);
+    this.reactContext = context;
     context.addActivityEventListener(activityEventListener);
   }
 
@@ -46,6 +57,13 @@ public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule 
     } else {
       promise.reject(String.valueOf(resultCode), "Unable to retrieve phone number");
     }
+  }
+
+  public static void sendEvent (String eventName, Object data) {
+    if (reactContext == null) {
+      return;
+    }
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, data);
   }
 
   @NonNull
@@ -70,5 +88,34 @@ public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule 
     } catch (Exception e) {
       promise.reject(e);
     }
+  }
+
+  @ReactMethod
+  public void getAppSignatures (Promise promise) {
+    AppSignatureHelper helper = new AppSignatureHelper(getReactApplicationContext());
+    WritableNativeArray array = new WritableNativeArray();
+    ArrayList<String> signatures = helper.getAppSignatures();
+    for (String signature : signatures) {
+      array.pushString(signature);
+    }
+    promise.resolve(array);
+  }
+
+  @ReactMethod
+  public void startSmsRetriever (Promise promise) {
+    SmsRetrieverClient client = SmsRetriever.getClient(getReactApplicationContext());
+    Task<Void> task = client.startSmsRetriever();
+    task.addOnSuccessListener(new OnSuccessListener<Void>() {
+      @Override
+      public void onSuccess(Void aVoid) {
+        promise.resolve(true);
+      }
+    });
+    task.addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(@NonNull Exception e) {
+        promise.reject(e);
+      }
+    });
   }
 }
