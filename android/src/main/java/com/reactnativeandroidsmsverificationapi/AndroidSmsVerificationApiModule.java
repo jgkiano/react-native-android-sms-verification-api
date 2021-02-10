b76bf34a8710f2c1ca7781f8bcc7ecc3f1805d88
Julius Kiano <jgkiano@gmail.com>
2021-02-11 01:59:53 +0300

    added startSmsUserConsent

--- android/src/main/java/com/reactnativeandroidsmsverificationapi/AndroidSmsVerificationApiModule.java
+++ android/src/main/java/com/reactnativeandroidsmsverificationapi/AndroidSmsVerificationApiModule.java
@@ -2,7 +2,12 @@ package com.reactnativeandroidsmsverificationapi;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
@@ -18,6 +23,8 @@ import com.google.android.gms.auth.api.credentials.Credentials;
 import com.google.android.gms.auth.api.credentials.HintRequest;
 import com.google.android.gms.auth.api.phone.SmsRetriever;
 import com.google.android.gms.auth.api.phone.SmsRetrieverClient;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -26,7 +33,13 @@ import java.util.ArrayList;
 
 public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule {
 
-  private ActivityEventListener activityEventListener = new ActivityEventListener() {
+  private int phoneNumberRequestCode = 420;
+  private int userConsentRequestCode = 69;
+  private Promise promise;
+  private static ReactApplicationContext reactContext;
+
+
+  private final ActivityEventListener activityEventListener = new ActivityEventListener() {
     @Override
     public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
       handleOnActivityResult(activity, requestCode, resultCode, data);
@@ -37,25 +50,59 @@ public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule 
     }
   };
 
-  private int phoneNumberRequestCode = 420;
-  private Promise promise;
-  private static ReactApplicationContext reactContext;
+  private final BroadcastReceiver smsVerificationReceiver = new BroadcastReceiver() {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+        Bundle extras = intent.getExtras();
+        Status smsRetrieverStatus = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
+        switch (smsRetrieverStatus.getStatusCode()) {
+          case CommonStatusCodes.SUCCESS:
+            try {
+              String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
+              if (message != null) {
+                AndroidSmsVerificationApiModule.sendEvent(Constant.SMS_RECEIVED, message);
+              } else {
+                Intent consentIntent = extras.getParcelable(SmsRetriever.EXTRA_CONSENT_INTENT);
+                getCurrentActivity().startActivityForResult(consentIntent, userConsentRequestCode);
+              }
+            } catch (ActivityNotFoundException e) {
+              // TODO: handle error
+              e.printStackTrace();
+            }
+            break;
+          case CommonStatusCodes.TIMEOUT:
+            AndroidSmsVerificationApiModule.sendEvent(Constant.SMS_ERROR, CommonStatusCodes.TIMEOUT);
+            break;
+        }
+      }
+    }
+  };
 
   AndroidSmsVerificationApiModule(ReactApplicationContext context) {
     super(context);
     this.reactContext = context;
     context.addActivityEventListener(activityEventListener);
+    IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+    getReactApplicationContext().registerReceiver(smsVerificationReceiver, intentFilter, SmsRetriever.SEND_PERMISSION, null);
   }
 
   private void handleOnActivityResult (Activity activity, int requestCode, int resultCode, Intent data) {
-    if (promise == null || requestCode != phoneNumberRequestCode) {
-      return;
+    if (requestCode == userConsentRequestCode) {
+      if (resultCode == Activity.RESULT_OK) {
+        String message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE);
+        AndroidSmsVerificationApiModule.sendEvent(Constant.SMS_RECEIVED, message);
+      } else {
+        AndroidSmsVerificationApiModule.sendEvent(Constant.SMS_ERROR, "Unable to retrieve SMS");
+      }
     }
-    if (resultCode == Activity.RESULT_OK) {
-      Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
-      promise.resolve(credential.getId());
-    } else {
-      promise.reject(String.valueOf(resultCode), "Unable to retrieve phone number");
+    if (requestCode == phoneNumberRequestCode && promise != null) {
+      if (resultCode == Activity.RESULT_OK) {
+        Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+        promise.resolve(credential.getId());
+      } else {
+        promise.reject(String.valueOf(resultCode), "Unable to retrieve phone number");
+      }
     }
   }
 
@@ -118,4 +165,29 @@ public class AndroidSmsVerificationApiModule extends ReactContextBaseJavaModule 
       }
     });
   }
+
+  @ReactMethod
+  public void startSmsUserConsent (String senderPhoneNumber, int userConsentRequestCode, Promise promise) {
+    this.userConsentRequestCode = userConsentRequestCode;
+    SmsRetrieverClient client = SmsRetriever.getClient(getReactApplicationContext());
+    Task<Void> task;
+    if (senderPhoneNumber == null) {
+      task = client.startSmsUserConsent(null);
+    } else {
+      task = client.startSmsUserConsent(senderPhoneNumber);
+    }
+    task.addOnSuccessListener(new OnSuccessListener<Void>() {
+      @Override
+      public void onSuccess(Void aVoid) {
+        promise.resolve(true);
+      }
+    });
+    task.addOnFailureListener(new OnFailureListener() {
+      @Override
+      public void onFailure(@NonNull Exception e) {
+        promise.reject(e);
+      }
+    });
+  }
+
 }
